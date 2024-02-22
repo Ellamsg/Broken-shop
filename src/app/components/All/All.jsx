6e42bf54170de43ba81b7e8c2fd61add882395ff
Ellams George <ellamsgeorge7@gmail.com>
2024-02-22 18:17:05 +0100

    broke

--- src/app/components/All/All.jsx
+++ src/app/components/All/All.jsx
@@ -23,7 +23,7 @@ const All = ({ product,imageStyle }) => {
           </div>
 
           <div className="md:text-[16px] text-[13px] flex gap-2 uppercase">
-            <p>{product?.name}</p>
+            <p className="md:text-[16px] text-[11px]">{product?.name}</p>
 
             <p>NGN{product?.price/ 100}</p>
           </div>
