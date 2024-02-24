8ad038c2b8119fe40dc9414b42009cdecec5c7f8
Ellams George <ellamsgeorge7@gmail.com>
2024-02-24 13:55:00 +0100

    insta

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -5,15 +5,15 @@ const Footer = () => {
         <div className="fixed  bottom-4 right-3 md:right-6">
             <img className="size-6" src="/icons/whatsapp2.png"/>
         </div>
-        <div className="flex py-4 mx-3    md:mx-[70px]  justify-between border-t-2 border-white">
+        <div className="flex py-4 mx-3  md:mx-[70px]  justify-between border-t-2 border-white">
 
             <div className="flex">
-                <p>instagram</p>
+                <p>Instagram</p>
                
             </div>
             
             <div>
-                <p>instagram</p>
+                <p>Instagram</p>
            
             </div>
         </div>
