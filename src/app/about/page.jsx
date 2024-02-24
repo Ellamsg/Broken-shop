fd95733a99e7ffe5d0001562013c7664bb3406f7
Ellams George <ellamsgeorge7@gmail.com>
2024-02-24 13:58:06 +0100

    fix

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,46 +1,47 @@
 const Page = () => {
-  return (
-    <div className="space py-6">
-      <div className="md:w-[60%]">
-        <p className="md:text-6xl text-4xl font-semibold">
-          ABOUT BROKEN SHOP, LAGOS NG
-        </p>
-      </div>
-
-      <div className="flex  md:justify-end  ">
-        <div className=" md:w-[35%] ">
-          <p className="py-3 text-3xl font-semibold">About the brand</p>
-          Welcome to Broken, your premier destination for stylish and affordable
-          clothing located in the vibrant heart of Lagos, Nigeria. At Broken, we
-          believe that fashion is not just about what you wear, but how you wear
-          it. Our brand ethos revolves around embracing individuality,
-          celebrating diversity, and empowering our customers to express
-          themselves through their clothing choices. Established with a passion
-          for providing trendy yet timeless pieces, Broken offers a curated
-          selection of apparel and accessories that cater to every style and
-          occasion. Whether you're searching for the perfect outfit for a night
-          out on the town or seeking everyday essentials with a unique flair,
-          we've got you covered.
-          <p className="py-4">
-          Inspired by the dynamic energy and cultural
-          richness of Lagos, our collections blend contemporary trends with
-          traditional craftsmanship, resulting in garments that are both
-          fashion-forward and rooted in authenticity.
+    return (
+      <div className="space py-6">
+        <div className="md:w-[60%]">
+          <p className="md:text-6xl text-4xl font-semibold">
+            ABOUT BROKEN SHOP, LAGOS NG
           </p>
-           From casual streetwear to
-          elegant evening ensembles, each piece in our range is thoughtfully
-          designed to make a statement and elevate your wardrobe. More than just
-          a clothing store, Broken is a community-driven space where fashion
-          enthusiasts come together to explore, create, and inspire. We're
-          committed to fostering a supportive environment where everyone feels
-          welcome and encouraged to embrace their individual style journey.
-          Thank you for choosing Broken as your go-to destination for all things
-          fashion. Join us as we redefine the way you shop for clothes and wears
-          in Lagos, one stylish piece at a time.
+        </div>
+  
+        <div className="flex  md:justify-end  ">
+          <div className=" md:w-[35%] ">
+            <p className="py-3 text-3xl font-semibold">About the brand</p>
+            Welcome to Broken, your premier destination for stylish and affordable
+            clothing located in the vibrant heart of Lagos, Nigeria. At Broken, we
+            believe that fashion is not just about what you wear, but how you wear
+            it. Our brand ethos revolves around embracing individuality,
+            celebrating diversity, and empowering our customers to express
+            themselves through their clothing choices. Established with a passion
+            for providing trendy yet timeless pieces, Broken offers a curated
+            selection of apparel and accessories that cater to every style and
+            occasion. Whether you&apos;re searching for the perfect outfit for a night
+            out on the town or seeking everyday essentials with a unique flair,
+            we&apos;ve got you covered.
+            <p className="py-4">
+            Inspired by the dynamic energy and cultural
+            richness of Lagos, our collections blend contemporary trends with
+            traditional craftsmanship, resulting in garments that are both
+            fashion-forward and rooted in authenticity.
+            </p>
+             From casual streetwear to
+            elegant evening ensembles, each piece in our range is thoughtfully
+            designed to make a statement and elevate your wardrobe. More than just
+            a clothing store, Broken is a community-driven space where fashion
+            enthusiasts come together to explore, create, and inspire. We&apos;re
+            committed to fostering a supportive environment where everyone feels
+            welcome and encouraged to embrace their individual style journey.
+            Thank you for choosing Broken as your go-to destination for all things
+            fashion. Join us as we redefine the way you shop for clothes and wears
+            in Lagos, one stylish piece at a time.
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Page;
+    );
+  };
+  
+  export default Page;
+  
