489f372fd9c28bfd0518934b1f4c57402da60cce
Ellams George <ellamsgeorge7@gmail.com>
2024-03-08 18:20:14 +0100

    ship

---
+++ src/app/shipping/page.jsx
@@ -0,0 +1,25 @@
+const Page = () => {
+    return (
+      <div className="space py-6 ">
+        <div className="md:w-[60%]">
+          <p className="md:text-6xl  text-4xl font-semibold">
+          Shipping and Returns
+          </p>
+        </div>
+  
+        <div className="flex  md:justify-end  ">
+          <div className=" md:w-[35%] ">
+          All Sales Final: Once a purchase is made, it is considered final. We do not offer refunds or exchanges.
+<br/>Incorrect Item: If you receive an incorrect item, please contact us at brokenbycoker@gmail.com.
+<br/>Sale Items: Items purchased during a sale are non-refundable and cannot be returned.
+<br/>Non-Brokenby Coker Purchases: We cannot accept returns for Brokenby Coker products that were not purchased directly from our website, www.brokenbycoker.com.
+<br/>Customs and Import Duties: Any customs, import duties, brokerage fees, or taxes incurred during return shipments are the responsibility of the customer.
+<br/>Refused Packages: If you refuse a package, you will be responsible for all costs, duties, fees, or taxes associated with shipping it back to Brokenby Coker.
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Page;
+  
