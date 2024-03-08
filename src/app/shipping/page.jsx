f9f5021ddc4c4e7c193b1fc54ea1e8fa03ab9705
Ellams George <ellamsgeorge7@gmail.com>
2024-03-08 18:31:09 +0100

    push

--- src/app/shipping/page.jsx
+++ src/app/shipping/page.jsx
@@ -12,7 +12,7 @@ const Page = () => {
           All Sales Final: Once a purchase is made, it is considered final. We do not offer refunds or exchanges.
 <br/>Incorrect Item: If you receive an incorrect item, please contact us at brokenbycoker@gmail.com.
 <br/>Sale Items: Items purchased during a sale are non-refundable and cannot be returned.
-<br/>Non-Brokenby Coker Purchases: We cannot accept returns for Brokenby Coker products that were not purchased directly from our website, www.brokenbycoker.com.
+<br/>Non-brokenbycoker Purchases: We cannot accept returns for brokenbycoker products that were not purchased directly from our website, www.brokenbycoker.com.
 <br/>Customs and Import Duties: Any customs, import duties, brokerage fees, or taxes incurred during return shipments are the responsibility of the customer.
 <br/>Refused Packages: If you refuse a package, you will be responsible for all costs, duties, fees, or taxes associated with shipping it back to Brokenby Coker.
           </div>
