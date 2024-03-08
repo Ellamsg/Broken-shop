dcd20163eb141d08731a82ea4c748279bd35f79d
Ellams George <ellamsgeorge7@gmail.com>
2024-03-08 22:19:55 +0100

    remove favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,12 @@ import Header from "./components/header/header";
 import AuthProvider from "./components/AuthProvider/AuthProvider";
 import Footer from "./components/footer/Footer";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Brokenbycoker",
   description: "E-commerce fashion",
+  
 };
 
 export default function RootLayout({ children }) {
@@ -28,17 +28,15 @@ export default function RootLayout({ children }) {
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
-<link rel="icon" href="/images/brokenheart.png" sizes="any" />
-        
+        <link rel="icon" href="/images/brokenheart.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <AuthProvider>
           <div>
-        
             <Header />
             <div className="">{children}</div>
 
-           <Footer/>
+            <Footer />
           </div>
         </AuthProvider>
       </body>
