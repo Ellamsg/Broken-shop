2aa97c3ccb666023aaaaff8049e884e4ed4b6f28
Ellams George <ellamsgeorge7@gmail.com>
2024-03-09 21:43:06 +0100

    broken

--- src/app/policies/page.jsx
+++ src/app/policies/page.jsx
@@ -1,40 +1,45 @@
 const Page = () => {
-    return (
-      <div className="space py-6 ">
-        <div className="md:w-[60%]">
-          <p className="md:text-6xl  text-4xl font-semibold">
-          Privacy Policy
+  return (
+    <div className="space py-6 ">
+      <div className="md:w-[60%]">
+        <p className="md:text-6xl  text-4xl font-semibold">Privacy Policy</p>
+      </div>
+
+      <div className="flex  md:justify-end  ">
+        <div className=" md:w-[35%] ">
+        <a className="underline" href="www.brokenbycoker.com"> www.brokenbycoker.com</a>  is committed to protecting your privacy and
+          maintaining the security of any personal information received from
+          you. We strictly adhere to the requirements of the data protection
+          legislation in Nigeria. The purpose of this statement is to explain to
+          you what personal information we collect and how we may use it. When
+          you order, we need to know your name, address, and email address. This
+          allows us to process and fulfil your order. You have the option to
+          withhold personal information that is not required for the order
+          process. We use your personal information for payment and processing
+          of your order purposes.
+          <p className="py-3">
+            We may use your contact details to keep you updated about promotions
+            or offers that we think will be of interest to you. We follow strict
+            security procedures in the storage and disclosure of information
+            that you have given us, to prevent unauthorized access in accordance
+            with the Nigeria data protection legislation. We do not collect
+            sensitive information about you except when you specifically
+            knowingly provide it. In order to maintain the accuracy of our
+            database, you can check, update or remove your personal details by
+            contacting us by email. We do not store your credit card or
+            financial information on our website. In order to process
+            credit/debit card transactions, the bank or card processing agency
+            may require verifying your personal details for authorization
+            outside of Nigeria. Your information will not be transferred outside
+            Nigeria for any other purpose. If you have any questions about
+            privacy please contact us by email at brokenbycoker@gmail.com We
+            create art through fashion, experiences and imagery inspired by
+            Africa’s evolving identity. Lagos based..
           </p>
         </div>
-  
-        <div className="flex  md:justify-end  ">
-          <div className=" md:w-[35%] ">
-          
-            www.brokenbycoker.com is committed to protecting your privacy and maintaining the security of any personal information received from you. We strictly adhere to the requirements of the data protection legislation in Nigeria. The purpose of this statement is to explain to you what personal information we collect and how we may use it.
-
-When you order, we need to know your name, address, and email address. This allows us to process and fulfil your order. You have the option to withhold personal information that is not required for the order process.
-
-We use your personal information for payment and processing of your order purposes.
-<p className="py-3">
-We may use your contact details to keep you updated about promotions or offers that we think will be of interest to you.
-
-We follow strict security procedures in the storage and disclosure of information that you have given us, to prevent unauthorized access in accordance with the Nigeria data protection legislation.
-
-We do not collect sensitive information about you except when you specifically knowingly provide it. In order to maintain the accuracy of our database, you can check, update or remove your personal details by contacting us by email.
-
-We do not store your credit card or financial information on our website.
-
-In order to process credit/debit card transactions, the bank or card processing agency may require verifying your personal details for authorization outside of Nigeria. Your information will not be transferred outside Nigeria for any other purpose. If you have any questions about privacy please contact us by email at brokenbycoker@gmail.com
-
-We create art through fashion, experiences and imagery inspired by Africa’s evolving identity. Lagos based..
-</p>
-
-
-          </div>
-        </div>
       </div>
-    );
-  };
-  
-  export default Page;
-  
+    </div>
+  );
+};
+
+export default Page;
